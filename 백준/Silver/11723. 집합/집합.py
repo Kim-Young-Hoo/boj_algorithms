0e36688ce25a9b78e46f8c03c322f1ce93ea5659
KimYoungHoo <47880101+Kim-Young-Hoo@users.noreply.github.com>
2022-05-08 17:24:47 +0900

    [Silver V] Title: 집합, Time: 4304 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -2,31 +2,22 @@ import sys
 
 m = int(input())
 
-s = [False] * 20
-
-
-def solution(command):
-    global s
-
-    if command[0] == "add":
-        s[int(command[1]) - 1] = True
-
-    elif command[0] == "remove":
-        s[int(command[1]) - 1] = False
-
-    elif command[0] == "check":
-        print(int(s[int(command[1]) - 1]))
-
-    elif command[0] == "toggle":
-        s[int(command[1]) - 1] = not s[int(command[1]) - 1]
-
-    elif command[0] == "all":
-        s = [True] * 20
-
-    elif command[0] == "empty":
-        s = [False] * 20
-
-
+s = 0
 for _ in range(m):
     command = list(sys.stdin.readline().split())
-    solution(command)
+
+    if command[0] == "add":
+        s = s | (1 << int(command[1]) - 1)
+    elif command[0] == "remove":
+        s = s & ~(1 << int(command[1]) - 1)
+    elif command[0] == "check":
+        if s & (1 << int(command[1]) - 1):
+            print(1)
+        else:
+            print(0)
+    elif command[0] == "toggle":
+        s = s ^ (1 << int(command[1]) - 1)
+    elif command[0] == "all":
+        s = 2 ** 20 - 1
+    else:
+        s = 0
