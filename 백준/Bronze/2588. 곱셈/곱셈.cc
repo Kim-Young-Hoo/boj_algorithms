78c7ecf3a00bbfda6576f22a03e87f3c0bd59048
KimYoungHoo <47880101+Kim-Young-Hoo@users.noreply.github.com>
2022-05-09 19:05:24 +0900

    [Bronze IV] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a * (b % 10) << endl;
+    cout << a * ((b % 100) / 10) << endl;
+    cout << a * ((b % 1000) / 100) << endl;
+    cout << a * b;
+
+    return 0;
+}
