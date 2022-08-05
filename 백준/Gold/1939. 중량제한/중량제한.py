22571cf8ffe985fbf894b043d52f43f86c48ef0f
KimYoungHoo <47880101+Kim-Young-Hoo@users.noreply.github.com>
2022-08-05 18:07:28 +0900

    [Gold IV] Title: 중량제한, Time: 564 ms, Memory: 49264 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.py
+++ 백준/Gold/1939. 중량제한/중량제한.py
@@ -1,13 +1,13 @@
+import sys
 from collections import deque
 
 n, m = map(int, input().split(' '))
-# matrix = [[0] * (n + 1) for _ in range(n + 1)]
 
 graph = {i: {} for i in range(1, n + 1)}
 max_val = 0
 
 for _ in range(m):
-    a, b, c = map(int, input().split(' '))
+    a, b, c = map(int, sys.stdin.readline().split())
     if b in graph[a].keys():
         graph[a][b] = max(graph[a][b], c)
     else:
